c1fc4ffbc28a61ce979a3837981d8eda7760693a
tulunadrockers <tulunadrockers2@gmail.com>
2026-04-27 17:03:06 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import './App.css'; 
+import FigureList from './FigureList'; 
+ 
+function App() { 
+  return ( 
+    <div className="App"> 
+      <h1>Figure Gallery</h1> 
+      <FigureList /> 
+    </div> 
+  ); 
+} 
